0920157c041a8fdf6555a6ef62311dc2a06582d2
tsai <mengjietsai@163.com>
2020-04-24 17:44:04 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+// 引入页面初始化文件
+import './assets/style/reset.css'
+// 解决一像素边框问题
+import './assets/style/border.css'
+// 解决300毫秒点击延迟
+import fastClick from 'fastclick'
+fastClick.attach(document.body)
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
---
+++ src/views/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>shoye</div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      key: 'value'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
